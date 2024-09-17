41bd6e5c680c463390c9071a77e512993b1a84ba
mailvelous <mailvelous0@gmail.com>
2024-09-17 21:02:32 +0700

    adding static assets, image

--- route/index.route.go
+++ route/index.route.go
@@ -1,17 +1,24 @@
 package route
 
-import(
-	"github.com/gofiber/fiber/v2"
+import (
+	"restapi-gorm-gofiber/config"
 	"restapi-gorm-gofiber/handler"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 
 func RouteInit(r *fiber.App) {
+
+
 	r.Get ("/user", handler.UserHandlerGetAll)
 	r.Get ("/user/:id", handler.UserHandlerGetById)
 	r.Post ("/user", handler.UserHandlerCreate)
 	r.Put ("/user/:id", handler.UserHandlerUpdate)
 	r.Put ("/user/:id/updateEmail", handler.UserHandlerUpdateEmail)
 	r.Delete ("/user/:id", handler.UserHandlerDelete)
+	r.Static("/public", config.ProjectRootPath+"/public/assets")
 
+
+	
 }
